1c5ce02b86f48e3d154e1398fa41425e97f068f5
konstin <konstin@mailbox.org>
2023-05-23 15:59:02 +0200

    Add script that updates the version in the README
    This automates updating the version in README.md after a new tag was received

---
+++ update_version.py
@@ -0,0 +1,24 @@
+"""Updates the version in the readme and git commit."""
+
+import re
+from pathlib import Path
+from subprocess import check_call, check_output
+
+
+def main():
+    readme_md = Path("README.md")
+    readme = readme_md.read_text()
+    rev = Path(".version").read_text().strip()
+    readme = re.sub("rev: .*", f"rev: v{rev}", readme)
+    readme_md.write_text(readme)
+    # Only commit on change
+    # https://stackoverflow.com/a/9393642/3549270
+    if check_output(["git", "status", "-s"]).strip():
+        check_call(["git", "add", readme_md])
+        check_call(["git", "commit", "-m", f"Bump README.md version to {rev}"])
+    else:
+        print("No change")
+
+
+if __name__ == "__main__":
+    main()
