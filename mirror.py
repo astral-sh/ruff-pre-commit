ba4269e9c6827628d9ff4732249ba22fbde46ae7
Subin Kim <32478597+sudosubin@users.noreply.github.com>
2023-09-27 00:38:22 +0900

    feat: Update pre-commit mirror script (#53)
    <!--
    Thank you for contributing to Ruff! To help us out with reviewing,
    please consider the following:
    
    - Does this pull request include a summary of the change? (See below.)
    - Does this pull request include a descriptive title?
    - Does this pull request include references to any relevant issues?
    -->
    
    ## Summary
    
    <!-- What's the purpose of the change? What does it do, and why? -->
    
    - `pre-commit-mirror-maker` is not suitable for generating multiple
    hooks in single `.pre-commit-hooks.yaml` file.
    - So replaced the script and github workflows.
    
    ## Test Plan
    
    <!-- How was it tested? -->
    
    - Update `pyproject.toml`'s `ruff==0.0.290` into `ruff==0.0.289`, and
    run the script.
    
    ```sh
    $ python3 mirror.py
    ...
    ```

---
+++ mirror.py
@@ -0,0 +1,75 @@
+import re
+import subprocess
+import typing
+from pathlib import Path
+
+import tomllib
+import urllib3
+from packaging.requirements import Requirement
+from packaging.version import Version
+
+
+def main():
+    with open(Path(__file__).parent / "pyproject.toml", "rb") as f:
+        pyproject = tomllib.load(f)
+
+    all_versions = get_all_versions()
+    current_version = get_current_version(pyproject=pyproject)
+    target_versions = [v for v in all_versions if v > current_version]
+
+    for version in target_versions:
+        paths = process_version(version)
+        if subprocess.check_output(["git", "status", "-s"]).strip():
+            subprocess.run(["git", "add", *paths])
+            subprocess.run(["git", "commit", "-m", f"Mirror: {version}"])
+            subprocess.run(["git", "tag", f"v{version}"])
+        else:
+            print(f"No change v{version}")
+
+
+def get_all_versions() -> typing.List[Version]:
+    response = urllib3.request("GET", "https://pypi.org/pypi/ruff/json")
+    if response.status != 200:
+        raise RuntimeError("Failed to fetch versions from pypi")
+
+    versions = [Version(release) for release in response.json()["releases"]]
+    return sorted(versions)
+
+
+def get_current_version(pyproject: dict) -> Version:
+    requirements = [Requirement(d) for d in pyproject["project"]["dependencies"]]
+    requirement = next((r for r in requirements if r.name == "ruff"), None)
+    assert requirement is not None, "pyproject.toml does not have ruff requirement"
+
+    specifiers = list(requirement.specifier)
+    assert (
+        len(specifiers) == 1 and specifiers[0].operator == "=="
+    ), f"ruff's specifier should be exact matching, but `{requirement}`"
+
+    return Version(specifiers[0].version)
+
+
+def process_version(version: Version) -> typing.Sequence[str]:
+    def replace_pyproject_toml(content: str) -> str:
+        return re.sub(r'"ruff==.*"', f'"ruff=={version}"', content)
+
+    def replace_readme_md(content: str) -> str:
+        content = re.sub(r"rev: v\d+\.\d+\.\d+", f"rev: v{version}", content)
+        return re.sub(r"/ruff/\d+\.\d+\.\d+\.svg", f"/ruff/{version}.svg", content)
+
+    paths = {
+        "pyproject.toml": replace_pyproject_toml,
+        "README.md": replace_readme_md,
+    }
+
+    for path, replacer in paths.items():
+        with open(path) as f:
+            content = replacer(f.read())
+        with open(path, mode="w") as f:
+            f.write(content)
+
+    return tuple(paths.keys())
+
+
+if __name__ == "__main__":
+    main()
